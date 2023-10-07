56e38e7132818c896e4453084145026e7a14ce2f
lee14k <kmkovach@mtu.edu>
2023-10-07 10:31:53 -0400

    trying to test

--- pages/Store.js
+++ pages/Store.js
@@ -1,15 +1,2 @@
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import {ProductBrowser} from '@ecwid/nextjs-ecwid-plugin'
 
-export default function Store () {
-    return (
-        <div>
-            <Navbar/>
-  <ProductBrowser
-        storeId="7579020"
-      />
-      <Footer/>
-        </div>
-    )
 }
