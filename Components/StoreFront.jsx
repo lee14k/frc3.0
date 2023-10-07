41ecedf4dfcff11c9309dd38f507ffaa61e9fd1a
lee14k <kmkovach@mtu.edu>
2023-10-07 10:32:41 -0400

    trying to test

--- Components/StoreFront.jsx
+++ Components/StoreFront.jsx
@@ -1,15 +1,12 @@
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
+
 import ProductBrowser from "./ProductBrowser"
 
-export default function Store () {
+export default function StoreFront () {
     return (
         <div>
-            <Navbar/>
   <ProductBrowser
         storeId="7579020"
       />
-      <Footer/>
         </div>
     )
 }
