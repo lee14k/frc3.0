ee85442513e6c3b2b5282bf45deee529c9e10f67
lee14k <kmkovach@mtu.edu>
2023-11-10 15:12:40 -0500

    fixing navbar

--- Components/useHandleResize.jsx
+++ Components/useHandleResize.jsx
@@ -1,23 +1,16 @@
-'use client'
-
-import {useEffect, useState} from "react";
-
-
 const useHandleResize = (setIsMobile) => {
-    useEffect(() => {
+  useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 900);
     };
 
+    // Set initial state based on current window width
+    handleResize();
+
     // Add event listener to handle window resize
     window.addEventListener('resize', handleResize);
-
-    // Check initial window width on component mount
-    handleResize();
 
     // Cleanup the event listener on component unmount
     return () => window.removeEventListener('resize', handleResize);
   }, [setIsMobile]);
 }
-
-export default useHandleResize;
